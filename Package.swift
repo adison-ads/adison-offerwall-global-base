80309c128407bcd710cad84b5dab9d873d4c08bf
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-09-25 15:24:09 +0900

    0.9.0-beta1 릴리즈

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let version = "0.9.0-beta1"
-let checksum = ""
+let checksum = "a3a11de9ec18a3418335c92406d14a793e4d3bb9ecf0a6c77a68452f91e8f45b"
 
 let package = Package(
     name: "AdisonOfferwallGlobalBase",
@@ -12,10 +12,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AdisonOfferwallGlobalBase",
-            targets: ["AdisonOfferwallGlobalBase"]),
+            targets: ["AdisonOfferwallGlobalBase", "AdisonOfferwallGlobalBaseTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adison-ads/adison-offerwall-core", exact: "0.28.0-beta1")
+        .package(url: "https://github.com/adison-ads/adison-offerwall-global-core", exact: "0.28.0-beta1")
     ],
     targets: [
         .binaryTarget(
@@ -26,7 +26,7 @@ let package = Package(
         .target(
             name: "AdisonOfferwallGlobalBaseTarget",
             dependencies: [
-                .product(name: "AdisonOfferwallGlobalBase", package: "adison-offerwall-global-base")
+                .product(name: "AdisonOfferwallGlobalCore", package: "adison-offerwall-global-core")
             ],
             path: "Sources"
         )

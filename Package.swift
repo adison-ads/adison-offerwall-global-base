f1997b523e6ab5062517b3e57b378d46de256f4a
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-10-07 18:26:12 +0900

    0.9.2 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "0.9.0"
-let checksum = "1ed31b7f0eda5334967c13f04e677a937123876e92f6aeb7c2ec1be1a76c4943"
+let version = "0.9.2"
+let checksum = "b489ed25452ad6ad722e977c876ef5c3c934b9a28a27af0b772a6f2dcd47c104"
 
 let package = Package(
     name: "AdisonOfferwallGlobalBase",
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["AdisonOfferwallGlobalBase", "AdisonOfferwallGlobalBaseTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adison-ads/adison-offerwall-global-core", exact: "0.28.0")
+        .package(url: "https://github.com/adison-ads/adison-offerwall-global-core", exact: "0.28.2")
     ],
     targets: [
         .binaryTarget(
